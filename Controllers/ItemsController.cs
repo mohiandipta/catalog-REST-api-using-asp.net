75ce2d4f37d48b781bb61e0a12d2a650e73cb364
Md. Mohian Mustafa <intern2.afbl@akij.net>
2021-10-24 15:25:51 +0600

    install react.js as a view

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using catalog.Entities;
+using catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        // GET/item
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+    }
+}
