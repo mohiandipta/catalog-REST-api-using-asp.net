d7b9da0efc3ad3cea09ae76b21e7c6f0452fec6d
Md. Mohian Mustafa <intern2.afbl@akij.net>
2021-10-24 12:44:15 +0600

    Item added to the repositories/InMemItemsRepository

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using catalog.Entities;
 
 namespace catalog.Repositories
@@ -12,5 +13,14 @@ namespace catalog.Repositories
             new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 255, CreatedDate = DateTimeOffset.UtcNow },
             new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 200, CreatedDate = DateTimeOffset.UtcNow }
         };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
     }
 }
